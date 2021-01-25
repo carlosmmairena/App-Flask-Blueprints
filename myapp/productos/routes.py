24464ac3cf2789031528f47a187aad3da5eb8df9
Carlos Mairena <carlosmiguelmairena@gmail.com>
2021-01-24 18:20:18 -0600

    Listo a desplegar

--- myapp/productos/routes.py
+++ myapp/productos/routes.py
@@ -2,9 +2,14 @@ from flask import render_template
 from . import productos
 
 @productos.route('/', methods=['GET'])
+def home():
+    return 'Hola :)'
+
+
 @productos.route('/productos', methods=['GET', 'POST'])
 def mostrar_productos():
     return render_template('productos.html')
+
 
 @productos.route('/registrar-producto', methods=['GET', 'POST'])
 def guardar_producto():
--- setup.py
+++
@@ -1,4 +0,0 @@
-from myapp import app
-
-if __name__ == "__main__":
-    app.run()
